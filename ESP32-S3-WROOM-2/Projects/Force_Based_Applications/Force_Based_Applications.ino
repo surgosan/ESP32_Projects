a5d9265cc92172bf11395c56b8b9a3f4f2258ea2
surgosan <sergy200118@gmail.com>
2024-05-29 23:32:11 -0400

    S3 Force Resistor | S3 Force Based Applications WIP

---
+++ ESP32-S3-WROOM-2/Projects/Force_Based_Applications/Force_Based_Applications.ino
@@ -0,0 +1,21 @@
+#define fsrPin 4
+#define transistorPIN 5
+
+void setup() {
+  pinMode(fsrPin, INPUT);
+  pinMode(transistorPIN, OUTPUT);
+  digitalWrite(transistorPIN, LOW);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int fsrValue = analogRead(fsrPin);
+  if(fsrValue > 4000) {
+    digitalWrite(transistorPIN, HIGH);
+  } else {
+    digitalWrite(transistorPIN, LOW);
+  }
+  Serial.print("FSR Value: ");
+  Serial.println(fsrValue);
+  delay(500); // Delay for stability
+}
