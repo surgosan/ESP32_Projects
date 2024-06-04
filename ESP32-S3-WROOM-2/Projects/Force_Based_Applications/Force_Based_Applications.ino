5bfb56d3691fcbb2384b9df30fa33aef61815bf8
surgosan <sergy200118@gmail.com>
2024-06-03 22:31:15 -0400

    Force Based Applications Minor Change

--- ESP32-S3-WROOM-2/Projects/Force_Based_Applications/Force_Based_Applications.ino
+++ ESP32-S3-WROOM-2/Projects/Force_Based_Applications/Force_Based_Applications.ino
@@ -10,8 +10,8 @@ void setup() {
 
 void loop() {
   int fsrValue = analogRead(fsrPin);
-  if(fsrValue < 3500) {
-    digitalWrite(transistorPIN, HIGH);
+  if(fsrValue < 4000) {
+    digitalWrite(transistorPIN, HIGH); 
   } else {
     digitalWrite(transistorPIN, LOW);
   }
